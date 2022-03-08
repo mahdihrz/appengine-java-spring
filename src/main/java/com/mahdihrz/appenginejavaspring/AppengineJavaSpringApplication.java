ed182a6573aaa8cfc79643d64b4eebd853c6201f
Mahdi HARZALLAH <mahdihrz@gmail.com>
2022-03-08 11:30:24 +0100

    first generated sprint source

---
+++ src/main/java/com/mahdihrz/appenginejavaspring/AppengineJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mahdihrz.appenginejavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppengineJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppengineJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahdihrz/appenginejavaspring/AppengineJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahdihrz.appenginejavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppengineJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
